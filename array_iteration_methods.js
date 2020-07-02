95830d7c25dab746deb9f9ea66cdce975aa3e4ee
Alan Caruso <alan.carusoo@gmail.com>
2020-07-01 23:59:10 -0300

    Curso Js - Array Iteration

---
+++ array_iteration_methods.js
@@ -0,0 +1,13 @@
+var articulos = [
+  { nombre: "Bici", costo: 3000 },
+  { nombre: "TV", costoCash: 2500 },
+  { nombre: "Libro", costoCash: 320 },
+  { nombre: "Celular", costoCash: 10000 },
+  { nombre: "Laptop", costoCash: 20000 },
+  { nombre: "Teclado", costoCash: 500 },
+  { nombre: "Audifonos", costoCash: 1700 },
+];
+
+var articulosFiltrados = articulos.filter(function (articulo) {
+  return articulo.costo <= 500;
+});
